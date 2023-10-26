4abefeee84b3a1b4d65986738f4d935cf4c4e0d8
Prica277 <prica277@hsd.k12.or.us>
2023-10-26 09:41:36 -0700

    first commit

---
+++ cookie_dictionary/cookie_dictionary.py
@@ -0,0 +1,6 @@
+""" 
+Filename: cookie_dictionary.py
+Author: Alex Price
+Description:  Designed to speed along the baking process by allowing bakers to find the desired recipe quickly and easily. 
+"""
+
